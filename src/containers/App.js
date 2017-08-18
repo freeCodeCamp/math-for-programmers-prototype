81e9fdef32a917cd84663c83a5965174187b99fc
Juan D. Acosta <juandacorias@gmail.com>
2017-08-18 10:58:58 -0500

    Added docs build for GitHub pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Math LaTeX</h1>
+        <h1>Math LaTeX Challenges</h1>
         <Editor />
         <Preview />
       </div>
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -25,7 +25,8 @@ export default function register() {
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+      // serve assets;
+      // see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
 
@@ -36,7 +37,8 @@ export default function register() {
         // Is not local host. Just register service worker
         registerValidSW(swUrl);
       } else {
-        // This is running on localhost. Lets check if a service worker still exists or not.
+        // This is running on localhost.
+        // Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
       }
     });
